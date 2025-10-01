83c1b599c82ed146b44a4111d8f0e61708c31233
Neely Hopkins <nmhops@icloud.com>
2025-10-01 20:01:29 +0200

    rough draft footer

---
+++ js/footer.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('../components/footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data;
+    })
+    .catch(err => console.error("Failed to load footer:", err));
+});
